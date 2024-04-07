8ee0b6c6fba18b21e82c8795e013b7250698eea0
Madhav286 <164131589+Madhav286@users.noreply.github.com>
2024-04-07 15:43:27 +0530

    Add files via upload

---
+++ Welcometoguvi.java
@@ -0,0 +1,10 @@
+package javabasics.OOPS;
+
+public class Welcometoguvi {
+    public static void main(String[] args) {
+        String message = "Welcome to Guvi";
+        for(int i=0; i<=10;i++){
+            System.out.println(message);
+        }
+    }
+}
