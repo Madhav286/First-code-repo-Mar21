186f4d89061ebe4a14634b9b04668df3396ff669
Madhav286 <164131589+Madhav286@users.noreply.github.com>
2024-04-07 14:51:29 +0530

    Add files via upload

---
+++ Alphabets.java
@@ -0,0 +1,9 @@
+package javabasics.OOPS;
+
+public class Alphabets {
+    public static void main(String[] args) {
+        for(char ch ='A'; ch<= 'z'; ch++){
+            System.out.println(ch+" ");
+        }
+    }
+}
