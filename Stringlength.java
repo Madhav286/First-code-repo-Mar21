69ee2f2987c0e9744938b31173399aee93de33fe
Madhav286 <164131589+Madhav286@users.noreply.github.com>
2024-04-07 15:40:03 +0530

    Add files via upload

---
+++ Stringlength.java
@@ -0,0 +1,9 @@
+package javabasics.OOPS;
+
+public class Stringlength {
+    public static void main(String[] args) {
+        String msg = "Guvi Geek";
+        int length = msg.length();
+        System.out.println("Length of the string \"" + msg + "\" is: " + length);
+    }
+}
