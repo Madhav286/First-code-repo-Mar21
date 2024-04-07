d3ad50536d82d810ee41028f45f780ef26a08b08
Madhav286 <164131589+Madhav286@users.noreply.github.com>
2024-04-07 14:38:04 +0530

    Add files via upload

---
+++ Task8.java
@@ -0,0 +1,16 @@
+package javabasics.OOPS;
+
+public class Task8 {
+    public static void main(String[] args) {
+        // Declare four integer variables
+        int a = 5;
+        int b = 10;
+        int c = 3;
+        int d = 7;
+        
+        // Check if the sum of a and b is greater than the sum of c and d
+        if (a + b > c + d) {
+            System.out.println("The sum of a and b is greater than the sum of c and d");
+        }
+    }
+}
