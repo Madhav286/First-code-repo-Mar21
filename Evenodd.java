276a226652874e4a66f923819519f4ef558cc05d
Madhav286 <164131589+Madhav286@users.noreply.github.com>
2024-04-07 14:45:47 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,14 @@
+package javabasics.OOPS;
+
+public class Evenodd {
+    public static void main(String[] args) {
+        int a=10;
+        if(a%2==0){
+            System.out.println("The number is even");}
+
+            else{
+                System.out.println("The number is odd");
+            }
+        }
+    }
+
